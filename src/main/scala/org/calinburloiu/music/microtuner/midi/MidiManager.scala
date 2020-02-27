cb1c443ad1ba3e1c2c3f1823faa99583d5bdb52b
Calin-Andrei Burloiu <calin.burloiu@gmail.com>
2020-02-27 22:02:58 +0200

    Complete the code for MidiManager

--- src/main/scala/org/calinburloiu/music/microtuner/midi/MidiManager.scala
+++ src/main/scala/org/calinburloiu/music/microtuner/midi/MidiManager.scala
@@ -1,51 +1,161 @@
 package org.calinburloiu.music.microtuner.midi
 
-import javax.sound.midi.{MidiDevice, Receiver, Transmitter}
+import com.typesafe.scalalogging.StrictLogging
+import javax.sound.midi.{MidiDevice, MidiSystem, Receiver, Transmitter}
+import uk.co.xfactorylibrarians.coremidi4j.CoreMidiDeviceProvider
 
-import scala.collection.mutable
+import scala.collection.{mutable, GenSet}
 import scala.util.Try
 
-class MidiManager extends AutoCloseable {
+// TODO #1 Add logging!
+class MidiManager extends AutoCloseable with StrictLogging {
+  import MidiManager._
 
   private val inputDevicesInfo = mutable.Map[MidiDeviceId, MidiDevice.Info]()
-  private val openedInputDevices = mutable.Map[MidiDeviceId, (MidiDevice, Transmitter)]()
+  private val openedInputDevices = mutable.Map[MidiDeviceId, OpenedInputDevice]()
+
   private val outputDevicesInfo = mutable.Map[MidiDeviceId, MidiDevice.Info]()
-  private val openedOutputDevices = mutable.Map[MidiDeviceId, (MidiDevice, Receiver)]()
+  private val openedOutputDevices = mutable.Map[MidiDeviceId, OpenedOutputDevice]()
 
   refresh()
 
-  def midiDeviceInfo(midiDeviceId: MidiDeviceId): MidiDevice.Info = ???
+  def midiDeviceInfo(deviceId: MidiDeviceId): MidiDevice.Info =
+    inputDevicesInfo.getOrElse(deviceId, outputDevicesInfo(deviceId))
 
-  // TODO #1 Remember to remove opened devices that don't exist anymore
-  def refresh(): Unit = ???
+  def refresh(): Unit = {
+    // Alternative to `javax.sound.midi.MidiSystem.getMidiDeviceInfo()` to make Java MIDI work on Mac.
+    // This should also work on Windows.
+    val midiDeviceInfoArray = CoreMidiDeviceProvider.getMidiDeviceInfo
+    val devices = midiDeviceInfoArray.map(MidiSystem.getMidiDevice)
+
+    devices.foreach { device =>
+      val deviceInfo = device.getDeviceInfo
+      val maxTransmitters = device.getMaxTransmitters
+      val maxReceivers = device.getMaxReceivers
+
+      if (maxTransmitters == -1 /* unlimited */ || maxTransmitters > 0) {
+        inputDevicesInfo.update(MidiDeviceId(deviceInfo), deviceInfo)
+      }
+
+      if (maxReceivers == -1 /* unlimited */ || maxReceivers > 0) {
+        outputDevicesInfo.update(MidiDeviceId(deviceInfo), deviceInfo)
+      }
+    }
+
+    // TODO #1 Try to manually test this scenarios and see what happens
+    // Remove opened devices that were previously opened but now they don't appear anymore
+    def removeDisappearedOpenedDevices[D <: OpenedDevice](
+        openedDevices: mutable.Map[MidiDeviceId, D], deviceIds: GenSet[MidiDeviceId]): Unit = {
+      val disappearedOpenedDeviceIds = openedDevices.keySet diff deviceIds
+      disappearedOpenedDeviceIds.foreach { deviceId =>
+        val device = openedDevices(deviceId).midiDevice
+        Try(device.close()).recover {
+          case exception => logger.error(s"Failed to close disappeared device $deviceId", exception)
+        }
+        openedDevices.remove(deviceId)
+      }
+    }
+    removeDisappearedOpenedDevices(openedInputDevices, inputDevicesInfo.keySet)
+    removeDisappearedOpenedDevices(openedOutputDevices, outputDevicesInfo.keySet)
+  }
 
   def inputMidiDeviceIds: Seq[MidiDeviceId] = inputDevicesInfo.keys.toSeq
 
-  def openInput(midiDeviceId: MidiDeviceId): Try[Transmitter] = ???
+  def openInput(deviceId: MidiDeviceId): Try[Transmitter] = {
+    val deviceInfo = inputDevicesInfo(deviceId)
+    Try {
+      val device = MidiSystem.getMidiDevice(deviceInfo)
+      val transmitter = device.getTransmitter
 
-  def openFirstAvailableInput(midiDeviceIds: Seq[MidiDeviceId]): Option[MidiDeviceId] = ???
+      device.open()
+      openedInputDevices.update(deviceId, OpenedInputDevice(device, transmitter))
 
-  def isInputOpened(midiDeviceId: MidiDeviceId): Boolean = openedInputDevices.contains(midiDeviceId)
+      transmitter
+    }
+  }
 
-  def inputTransmitter(midiDeviceId: MidiDeviceId): Transmitter = openedInputDevices(midiDeviceId)._2
+  def openFirstAvailableInput(deviceIds: Seq[MidiDeviceId]): Option[MidiDeviceId] =
+    openFirstAvailableDevice(deviceIds, openInput)
 
-  def inputDevice(midiDeviceId: MidiDeviceId): MidiDevice = openedInputDevices(midiDeviceId)._1
+  def isInputOpened(deviceId: MidiDeviceId): Boolean = isDeviceOpened(deviceId, openedInputDevices)
 
-  def closeInput(midiDeviceId: MidiDeviceId): Try[Unit] = ???
+  def inputTransmitter(deviceId: MidiDeviceId): Transmitter = openedInputDevices(deviceId).transmitter
+
+  def inputDevice(deviceId: MidiDeviceId): MidiDevice = openedInputDevices(deviceId).midiDevice
+
+  def closeInput(deviceId: MidiDeviceId): Try[Unit] = closeDevice(deviceId, openedInputDevices)
 
   def outputMidiDeviceIds: Seq[MidiDeviceId] = outputDevicesInfo.keys.toSeq
 
-  def openOutput(midiDeviceId: MidiDeviceId): Try[Receiver] = ???
+  def openOutput(deviceId: MidiDeviceId): Try[Receiver] = {
+    val deviceInfo = outputDevicesInfo(deviceId)
+    Try {
+      val device = MidiSystem.getMidiDevice(deviceInfo)
+      val receiver = device.getReceiver
 
-  def openFirstAvailableOutput(midiDeviceIds: Seq[MidiDeviceId]): Option[MidiDeviceId] = ???
+      device.open()
+      openedOutputDevices.update(deviceId, OpenedOutputDevice(device, receiver))
 
-  def isOutputOpened(midiDeviceId: MidiDeviceId): Boolean = openedOutputDevices.contains(midiDeviceId)
+      receiver
+    }
+  }
 
-  def outputReceiver(midiDeviceId: MidiDeviceId): Receiver = openedOutputDevices(midiDeviceId)._2
+  def openFirstAvailableOutput(deviceIds: Seq[MidiDeviceId]): Option[MidiDeviceId] =
+    openFirstAvailableDevice(deviceIds, openOutput)
 
-  def outputDevice(midiDeviceId: MidiDeviceId): MidiDevice = openedOutputDevices(midiDeviceId)._1
+  def isOutputOpened(deviceId: MidiDeviceId): Boolean = isDeviceOpened(deviceId, openedOutputDevices)
 
-  def closeOutput(midiDeviceId: MidiDeviceId): Try[Unit] = ???
+  def outputReceiver(deviceId: MidiDeviceId): Receiver = openedOutputDevices(deviceId).receiver
 
-  override def close(): Unit = ???
+  def outputDevice(deviceId: MidiDeviceId): MidiDevice = openedOutputDevices(deviceId).midiDevice
+
+  def closeOutput(deviceId: MidiDeviceId): Try[Unit] = closeDevice(deviceId, openedOutputDevices)
+
+  override def close(): Unit = {
+    openedInputDevices.keys.foreach { deviceId =>
+      closeDevice(deviceId, openedInputDevices).recover {
+        case exception => logger.error(s"Failed to close input device $deviceId", exception)
+      }
+    }
+    openedOutputDevices.keys.foreach { deviceId =>
+      closeDevice(deviceId, openedOutputDevices).recover {
+        case exception => logger.error(s"Failed to close output device $deviceId", exception)
+      }
+    }
+  }
+
+  private def openFirstAvailableDevice(
+    deviceIds: Seq[MidiDeviceId], openFunc: MidiDeviceId => Try[_]): Option[MidiDeviceId] = {
+    deviceIds.toStream
+      .map { deviceId =>
+        (deviceId, openFunc(deviceId))
+      }
+      .find(_._2.isSuccess)
+      .map(_._1)
+  }
 }
+
+object MidiManager {
+
+  private def isDeviceOpened[D <: OpenedDevice](
+      deviceId: MidiDeviceId, openedDevices: mutable.Map[MidiDeviceId, D]): Boolean =
+    openedDevices.get(deviceId).exists(_.midiDevice.isOpen)
+
+  private def closeDevice[D <: OpenedDevice](
+      deviceId: MidiDeviceId, openedDevices: mutable.Map[MidiDeviceId, D]): Try[Unit] = Try {
+    // TODO #1 Do we need to close the Transmitter as well
+    openedDevices(deviceId).midiDevice.close()
+    openedDevices.remove(deviceId)
+  }
+
+  private def closeAllDevices[D <: OpenedDevice](
+      openedDevices: mutable.Map[MidiDeviceId, D], deviceIds: GenSet[MidiDeviceId]): Unit = {
+
+  }
+}
+
+sealed trait OpenedDevice {
+  val midiDevice: MidiDevice
+}
+case class OpenedInputDevice(override val midiDevice: MidiDevice, transmitter: Transmitter) extends OpenedDevice
+case class OpenedOutputDevice(override val midiDevice: MidiDevice, receiver: Receiver) extends OpenedDevice
