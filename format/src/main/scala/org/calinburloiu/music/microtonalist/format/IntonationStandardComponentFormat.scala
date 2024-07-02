0898a1973e6ea710cdfe8c7f96367e8cd63f3da5
Calin-Andrei Burloiu <calin.burloiu@gmail.com>
2024-07-02 08:16:57 +0300

    Add format for intonationStandard component

---
+++ format/src/main/scala/org/calinburloiu/music/microtonalist/format/IntonationStandardComponentFormat.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2024 Calin-Andrei Burloiu
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.calinburloiu.music.microtonalist.format
+
+import org.calinburloiu.music.microtonalist.core.{CentsIntonationStandard, EdoIntonationStandard, IntonationStandard, JustIntonationStandard}
+import play.api.libs.json.Json
+
+object IntonationStandardComponentFormat {
+  val ComponentFamilyName = "intonationStandard"
+
+  private val specs: ComponentJsonFormat.SpecsSeqType[IntonationStandard] = Seq(
+    ComponentJsonFormat.TypeSpec.withoutSettings(CentsIntonationStandard.typeName, CentsIntonationStandard),
+    ComponentJsonFormat.TypeSpec.withoutSettings(
+      JustIntonationStandard.typeName,
+      JustIntonationStandard
+    ),
+    ComponentJsonFormat.TypeSpec.withSettings[EdoIntonationStandard](
+      EdoIntonationStandard.typeName,
+      Json.format[EdoIntonationStandard],
+      classOf[EdoIntonationStandard]
+    )
+  )
+
+  def createComponentJsonFormat(): ComponentJsonFormat[IntonationStandard] = new ComponentJsonFormat(
+    ComponentFamilyName,
+    specs,
+    Some(CentsIntonationStandard.typeName)
+  )
+}
