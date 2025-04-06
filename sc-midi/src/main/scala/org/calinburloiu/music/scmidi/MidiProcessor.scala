9b7de86bff273274e101e42b27c038a124de6d28
Calin-Andrei Burloiu <calin.burloiu@gmail.com>
2025-04-06 14:46:49 +0300

    Change issue number from TODO

--- sc-midi/src/main/scala/org/calinburloiu/music/scmidi/MidiProcessor.scala
+++ sc-midi/src/main/scala/org/calinburloiu/music/scmidi/MidiProcessor.scala
@@ -18,7 +18,7 @@ package org.calinburloiu.music.scmidi
 
 import javax.sound.midi.{MidiMessage, Receiver, Transmitter}
 
-// TODO #132 Make a MidiProcessor be composed from a Receiver and a Transmitter rather than extend them to reduce
+// TODO #134 Make a MidiProcessor be composed from a Receiver and a Transmitter rather than extend them to reduce
 //  confusion
 
 /**
